5465ef65b6b5203071761cf096f0f29ddf2e477e
Harish Natarajan <48133371+N-Harish@users.noreply.github.com>
2026-09-12 10:54:14 +0530

    Clean up rmsnorm.py by removing commented code
    Removed commented-out code for the RMSNorm class.

--- src/llama3_2_1b_scratch/rmsnorm.py
+++ src/llama3_2_1b_scratch/rmsnorm.py
@@ -2,17 +2,6 @@ import torch
 from torch import nn
 
 
-# class RMSNorm(nn.Module):
-#     def __init__(self, d_model: int, eps: float = 1e-5):
-#         super().__init__()
-#         self.gamma = nn.Parameter(torch.ones(d_model))
-#         self.eps = eps
-
-#     def forward(self, x: torch.Tensor):
-#         mean_x_sq = torch.mean(x ** 2, dim=-1, keepdim=True)
-#         return x * torch.rsqrt(mean_x_sq + self.eps) * self.gamma
-
-
 class RMSNorm(nn.Module):
     def __init__(self, d_model, eps=1e-5):
         super().__init__()
